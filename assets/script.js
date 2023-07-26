5b1f1b64ba2bc04a1ae763e32878ac36882d4995
mkidee <mckaymemmott@gmail.com>
2023-07-26 09:26:14 -0600

    selectors created for js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 // API Key:
 const APIkey = "8d964863fc8c92fe7049a5f9808fd5e8";
 
-var searchBtn = document.querySelector('#searchBtn');
-var city = document.querySelector('#searchArea').textContent;
+// Global Selectors
+const input = document.querySelector('#city-input');
+const button = document.querySelector('#save-button');
+const cityList = document.querySelector('#city-list');
+const weatherContainer = document.querySelector("#weather-container");
+const forecastContainer = document.querySelector("#forecast-container");
+const weatherUL = document.querySelector("#weather-ul");
 
-
+// max number of cities to save & display
+const cityDisplayMax = 15;
