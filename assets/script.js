cc9966a91f4e948667998793beca77069d14d88e
mkidee <mckaymemmott@gmail.com>
2023-07-26 09:30:24 -0600

    on clicks/listeners, getting weather data by api

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,24 @@ const weatherUL = document.querySelector("#weather-ul");
 
 // max number of cities to save & display
 const cityDisplayMax = 15;
+
+// this listens for the enter key rather than a click because its more intuitive
+$("#city-input").keydown(function(event) {
+    if (event.keyCode === 13) {
+        // prevent default is needed because the save button will otherwise try to take in "city" as an object
+        event.preventDefault();
+        // if keydown detected, click save button
+        $("#save-button").click();
+    }
+});
+
+// this listens for a click on the Get Weather! button 
+$("#save-button").on("click", function (event) {
+    event.preventDefault();
+    // set city to the value entered into the input
+    const city = $("#city-input").val();
+    // get the weather, feed it city
+    getWeather(city);
+    // save the city to local storage
+    saveCity();
+})
